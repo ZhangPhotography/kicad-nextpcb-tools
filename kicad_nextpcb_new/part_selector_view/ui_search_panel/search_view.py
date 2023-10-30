10ce4a6b9b05f294f3a35a5a3a788afe7264d333
hafzhang <969484552@qq.com>
2023-10-30 14:21:57 +0800

    Feat/add-a-box-to-show-part-details-at-the-lower-part-of-mainwindow

---
+++ kicad_nextpcb_new/part_selector_view/ui_search_panel/search_view.py
@@ -0,0 +1,19 @@
+import wx
+import wx.xrc
+
+
+from .ui_search_panel import UiSearchPanel
+
+
+
+class SearchView(UiSearchPanel):
+    def __init__(self, parent, id=wx.ID_ANY, pos=wx.DefaultPosition, size=wx.DefaultSize, style=wx.TAB_TRAVERSAL, name=wx.EmptyString):
+        super().__init__(parent, id=id, pos=pos, size=size, style=style, name=name)
+
+
+        self.mpn_textctrl.SetHint("e.g. 123456")
+        self.manufacturer.SetHint("e.g. Vishay")
+
+    #     self.description.SetHint("e.g. 100nF")
+    #     self.package.SetHint("e.g. 0806")
+    #     self.search_button.SetBitmap( wx.Bitmap((self.GetImagePath("nextpcb-search.png")), wx.BITMAP_TYPE_ANY ) )
